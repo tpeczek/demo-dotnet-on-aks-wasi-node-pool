451c8576012f5b8bdefc925a6d7c89ae768177ab
Tomasz Pęczek <tpeczek@gmail.com>
2023-12-27 17:25:22 +0100

    Adding C# interop for provided C/C++ types

---
+++ src/Demo.Wasm.Slight/WasiInterop.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Demo.Wasm.Slight.Wasi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal readonly struct WasiString
+    {
+        private readonly nint _utf8Ptr;
+        private readonly int _utf8Length;
+
+        internal WasiString(nint ptr, int length)
+        {
+            _utf8Ptr = ptr;
+            _utf8Length = length;
+        }
+
+        public override string ToString() => Marshal.PtrToStringUTF8(_utf8Ptr, _utf8Length);
+
+        public static unsafe WasiString FromString(string value)
+        {
+            var exactByteCount = checked(Encoding.UTF8.GetByteCount(value));
+            var mem = Marshal.AllocHGlobal(exactByteCount);
+            var buffer = new Span<byte>((void*)mem, exactByteCount);
+            int byteCount = Encoding.UTF8.GetBytes(value, buffer);
+            return new WasiString(mem, byteCount);
+        }
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal readonly struct WasiError
+    {
+        [FieldOffset(0)]
+        private readonly byte _tag;
+
+        [FieldOffset(4)]
+        private readonly WasiString _errorWithDescription;
+
+        public WasiString ErrorWithDescription => _errorWithDescription;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal readonly struct WasiHttpRouter
+    {
+        private readonly uint _idx;
+
+        public readonly uint Index => _idx;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal readonly struct WasiHttpRouterOrError
+    {
+        [FieldOffset(0)]
+        private readonly bool _isError;
+
+        [FieldOffset(4)]
+        private readonly WasiHttpRouter _router;
+
+        [FieldOffset(4)]
+        private readonly WasiError _error;
+
+        public readonly bool IsError => _isError;
+
+        public readonly WasiHttpRouter? Router => _isError ? null : _router;
+
+        public readonly WasiError? Error => _isError ? _error : null;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal readonly struct WasiHttpServer
+    {
+        private readonly uint _idx;
+
+        public readonly uint Index => _idx;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal readonly struct WasiHttpServerOrError
+    {
+        [FieldOffset(0)]
+        private readonly bool _isError;
+
+        [FieldOffset(4)]
+        private readonly WasiHttpServer _server;
+
+        [FieldOffset(4)]
+        private readonly WasiError _error;
+
+        public readonly bool IsError => _isError;
+
+        public readonly WasiHttpServer? Server => _isError ? null : _server;
+
+        public readonly WasiError? Error => _isError ? _error : null;
+    }
+}
