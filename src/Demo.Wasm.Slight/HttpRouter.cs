1e15de1e3f1f2d00f17ce9fe059b546b25ac9c48
Tomasz Pęczek <tpeczek@gmail.com>
2023-12-28 13:38:32 +0100

    Implementing HttpServer

--- src/Demo.Wasm.Slight/HttpRouter.cs
+++ src/Demo.Wasm.Slight/HttpRouter.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using Demo.Wasm.Slight.Wasi;
+using System.Runtime.InteropServices;
 
 namespace Demo.Wasm.Slight
 {
@@ -8,5 +9,21 @@ namespace Demo.Wasm.Slight
         private readonly uint _idx;
 
         public readonly uint Index => _idx;
+
+        public static HttpRouter Create()
+        {
+            WasiExpected<HttpRouter> expected = new WasiExpected<HttpRouter>();
+
+            HttpRouterFunctions.New(ref expected);
+
+            if (expected.IsError)
+            {
+                throw new Exception(expected.Error?.ErrorWithDescription.ToString());
+            }
+
+#pragma warning disable CS8629 // Nullable value type may be null.
+            return (HttpRouter)expected.Result;
+#pragma warning restore CS8629 // Nullable value type may be null.
+        }
     }
 }
--- src/Demo.Wasm.Slight/HttpServer.cs
+++ src/Demo.Wasm.Slight/HttpServer.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using Demo.Wasm.Slight.Wasi;
+using System.Runtime.InteropServices;
 
 namespace Demo.Wasm.Slight
 {
@@ -8,5 +9,34 @@ namespace Demo.Wasm.Slight
         private readonly uint _idx;
 
         public readonly uint Index => _idx;
+
+        public static HttpServer Serve(string address, HttpRouter router)
+        {
+            WasiString wasiAddress = WasiString.FromString(address);
+            WasiExpected<HttpServer> expected = new WasiExpected<HttpServer>();
+
+            HttpServerFunctions.Serve(ref wasiAddress, router, ref expected);
+
+            if (expected.IsError)
+            {
+                throw new Exception(expected.Error?.ErrorWithDescription.ToString());
+            }
+
+#pragma warning disable CS8629 // Nullable value type may be null.
+            return (HttpServer)expected.Result;
+#pragma warning restore CS8629 // Nullable value type may be null.
+        }
+
+        public void Stop()
+        {
+            WasiExpected<HttpServer> expected = new WasiExpected<HttpServer>();
+
+            HttpServerFunctions.Stop(this, ref expected);
+
+            if (expected.IsError)
+            {
+                throw new Exception(expected.Error?.ErrorWithDescription.ToString());
+            }
+        }
     }
 }
