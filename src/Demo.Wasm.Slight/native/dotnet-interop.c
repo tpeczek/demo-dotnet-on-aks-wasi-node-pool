83807e6d7c19932c95009f38fe50d79711b96cbe
Tomasz Pęczek <tpeczek@gmail.com>
2023-12-28 12:49:11 +0100

    Attaching internal .NET calls to externally provided C functions

---
+++ src/Demo.Wasm.Slight/native/dotnet-interop.c
@@ -0,0 +1,13 @@
+﻿#include <mono-wasi/driver.h>
+#include "http.h"
+
+void attach_internal_calls() {
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpRouterFunctions::New", http_router_new);
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpRouterFunctions::Get", http_router_get);
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpRouterFunctions::Put", http_router_put);
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpRouterFunctions::Post", http_router_post);
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpRouterFunctions::Delete", http_router_delete);
+
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpServerFunctions::Serve", http_server_serve);
+    mono_add_internal_call("Demo.Wasm.Slight.Wasi.HttpServerFunctions::Stop", http_server_stop);
+}
