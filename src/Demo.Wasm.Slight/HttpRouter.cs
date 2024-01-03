b56686a4f6a64c72f4aa86e34286036d3c21940e
Tomasz Pęczek <tpeczek@gmail.com>
2024-01-03 11:23:29 +0000

    [ContainerD shims] Adding generic HTTP handler

--- src/Demo.Wasm.Slight/HttpRouter.cs
+++ src/Demo.Wasm.Slight/HttpRouter.cs
@@ -8,6 +8,7 @@ namespace Demo.Wasm.Slight
     {
         // Any library name that P/Invoke generator knows
         private const string LIBRARY_NAME = "libSystem.Native";
+        private static readonly WasiString REQUEST_HANDLER = WasiString.FromString("handle-http");
 
         private uint _index;
 
@@ -21,6 +22,38 @@ namespace Demo.Wasm.Slight
         public static HttpRouter Create()
         {
             http_router_new(out WasiExpected<uint> expected);
+
+            if (expected.IsError)
+            {
+                throw new Exception(expected.Error?.ErrorWithDescription.ToString());
+            }
+
+#pragma warning disable CS8629 // Nullable value type may be null.
+            return new HttpRouter(expected.Result.Value);
+#pragma warning restore CS8629 // Nullable value type may be null.
+        }
+
+        public HttpRouter RegisterRoute(HttpMethod method, string route)
+        {
+            WasiExpected<uint> expected;
+
+            switch (method)
+            {
+                case HttpMethod.GET:
+                    http_router_get(_index, WasiString.FromString(route), REQUEST_HANDLER, out expected);
+                    break;
+                case HttpMethod.PUT:
+                    http_router_put(_index, WasiString.FromString(route), REQUEST_HANDLER, out expected);
+                    break;
+                case HttpMethod.POST:
+                    http_router_post(_index, WasiString.FromString(route), REQUEST_HANDLER, out expected);
+                    break;
+                case HttpMethod.DELETE:
+                    http_router_delete(_index, WasiString.FromString(route), REQUEST_HANDLER, out expected);
+                    break;
+                default:
+                    throw new NotSupportedException($"The HTTP request method {method} is not supported.");
+            }
 
             if (expected.IsError)
             {
--- src/Demo.Wasm.Slight/native/dotnet-http-handler.c
+++ src/Demo.Wasm.Slight/native/dotnet-http-handler.c
@@ -1,5 +1,44 @@
-//#include <http.h>
+#include <string.h>
+#include <wasm/driver.h>
 #include "http-handler.h"
 
+int mono_runtime_loaded = 0;
+MonoMethod* handle_request_method;
+
+void mono_wasm_invoke_method_ref(MonoMethod* method, MonoObject** this_arg_in, void* params[], MonoObject** _out_exc, MonoObject** out_result);
+
 void http_handler_handle_http(http_handler_request_t* req, http_handler_expected_response_error_t* ret0) {
+    if (!mono_runtime_loaded) {
+        mono_wasm_load_runtime("", 0);
+
+        mono_runtime_loaded = 1;
+    }
+
+    if (!handle_request_method)
+    {
+        handle_request_method = lookup_dotnet_method("Demo.Wasm.Slight", "Demo.Wasm.Slight", "HttpServer", "HandleRequest", -1);
+    }
+
+    void* method_params[] = { req, ret0 };
+    MonoObject* exception;
+    MonoObject* result;
+    mono_wasm_invoke_method_ref(handle_request_method, NULL, method_params, &exception, &result);
+
+    if (exception) {
+        char* exception_string_utf8 = mono_string_to_utf8((MonoString*)result);
+        *ret0 = (http_handler_expected_response_error_t){
+            .is_err = 1,
+            .val = {
+                .err = {
+                    .tag = HTTP_HANDLER_ERROR_ERROR_WITH_DESCRIPTION,
+                    .val = {
+                        .error_with_description = {
+                            .ptr = exception_string_utf8,
+                            .len = strlen(exception_string_utf8)
+                        }
+                    }
+                }
+            }
+        };
+    }
 }
